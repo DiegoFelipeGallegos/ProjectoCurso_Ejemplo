91a2da7949b550882b03ac981754c79bd99f5077
Diego Gallegos <di.gallegoss@duocuc.cl>
2022-08-15 20:43:46 -0400

    se arreglo el navegador web y se agregaron nuevas páginas

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hola me falto este mensaje');
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
